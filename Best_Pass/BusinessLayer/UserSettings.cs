ed1fa33986d1809fa3df4003969de2aefc2ceba1
Vladimir <bratstakan@gmail.com>
2013-04-02 20:02:24 +0300

    [BusinessLayer] Создан класс настроик пользователя

---
+++ Best_Pass/BusinessLayer/UserSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Best_Pass.BusinessLayer
+{
+    public class UserSettings
+    {
+        private string _dbLocation;
+        public UserSettings()
+        {
+            DbLocation = @"D:\Учеба\Универ\6 курс\Диплом\Программа\GeneticEngine\GeneticEngine";
+        }
+
+        public string DbLocation
+        {
+            get { return _dbLocation; }
+            set 
+            { 
+                _dbLocation = value;
+                AppDomain.CurrentDomain.SetData("DataDirectory", _dbLocation);
+            }
+        }
+    }
+}
